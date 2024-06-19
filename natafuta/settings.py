756a0dfa7826ccc23cf6055af314ff1a075334ee
elyxkik <kikuniely@gmail.com>
2024-06-19 05:45:49 +0100

    Mis à jour

--- natafuta/settings.py
+++ natafuta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-os*(=34d0p!45chtkpcw2)xe^5vi(t0*xo*0$-5f-1521+z3zf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
